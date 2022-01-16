0d1d66de82db4f8cb0474b67123baade9ca0591b
fecalci <fcalciano@uade.edu.ar>
2022-01-15 22:04:28 -0300

    Se agregaron nuevas clases y gestión de JPA para las mismas

---
+++ src/main/java/com/api/market/persistence/entity/Cliente.java
@@ -0,0 +1,75 @@
+package com.api.market.persistence.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.math.BigInteger;
+import java.util.List;
+
+@Entity
+@Table(name="clientes")
+public class Cliente {
+
+    private String id;
+    private String nombre;
+    private String apellidos;
+    private BigInteger celular;
+    private String direccion;
+
+    @Column(name="correo_electronico")
+    private String correoElectronico;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Compra> compras;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public BigInteger getCelular() {
+        return celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setCelular(BigInteger celular) {
+        this.celular = celular;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+}
---
+++ src/test/java/com/api/market/ApiMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
